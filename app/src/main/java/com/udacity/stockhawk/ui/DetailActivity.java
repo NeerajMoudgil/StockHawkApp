781e6278e1092118d49f43f6ff74ad61e9279893
Neeraj Moudgil <neerajcse7@gmail.com>
2017-02-17 01:14:18 +0530

    improving Graph UI Xaxis-Part 3

--- app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.udacity.stockhawk.R;
 
@@ -19,6 +20,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         if (savedInstanceState == null) {
+            Log.d("saveinstanceate","null");
             Intent intent = getIntent();
             if (intent.hasExtra("symbol")) {
 
@@ -32,12 +34,6 @@ public class DetailActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.activity_detail, fragment)
                     .commit();
-
-
-
-
-
-
                 Timber.d("Symbol clicked: %s", symbol);
 
 
--- app/src/main/java/com/udacity/stockhawk/ui/DetailFragment.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailFragment.java
@@ -15,22 +15,30 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.google.common.collect.ImmutableList;
 import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.data.Contract;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
-public class DetailFragment extends Fragment implements  LoaderManager.LoaderCallbacks<Cursor>{
-    private static final String DETAIL_URI="DetailURI";
+public class DetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final String DETAIL_URI = "DetailURI";
     private LineChart lineChart;
     private LineData lineData;
     private static final int DETAIL_LOADER = 1;
+    final HashMap<Integer, String> labelMap = new HashMap<>();
+
 
     private static ImmutableList<String> DETAIL_COLUMNS = Contract.Quote.QUOTE_COLUMNS;
 
@@ -62,8 +70,8 @@ public class DetailFragment extends Fragment implements  LoaderManager.LoaderCal
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mUri=getArguments().getParcelable(DETAIL_URI);
-            Log.i("URIIRIRIIR",mUri.toString());
+            mUri = getArguments().getParcelable(DETAIL_URI);
+            Log.i("URIIRIRIIR", mUri.toString());
         }
 
     }
@@ -74,10 +82,12 @@ public class DetailFragment extends Fragment implements  LoaderManager.LoaderCal
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
         lineChart = (LineChart) rootView.findViewById(R.id.stock_chart);
         lineChart.setBackgroundColor(Color.TRANSPARENT);
+        Description description = new Description();
+        description.setText(getString(R.string.chart_descr));
+        lineChart.setDescription(description);
 
 
         return rootView;
-
 
 
     }
@@ -85,7 +95,7 @@ public class DetailFragment extends Fragment implements  LoaderManager.LoaderCal
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        if (  mUri !=null) {
+        if (mUri != null) {
 
             String[] projection = new String[DETAIL_COLUMNS.size()];
             projection = DETAIL_COLUMNS.toArray(projection);
@@ -105,22 +115,32 @@ public class DetailFragment extends Fragment implements  LoaderManager.LoaderCal
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
         if (data != null && data.moveToFirst()) {
             String stockData = data.getString(Contract.Quote.POSITION_HISTORY);
-            Log.d("got stoclkkksss",stockData);
+            Log.d("got stoclkkksss", stockData);
             String chartDataArr[] = stockData.split("\\r?\\n");
             List<Entry> entries = new ArrayList<Entry>();
-            int arrlength= chartDataArr.length;
+            int arrlength = chartDataArr.length;
 
 
-            for(int iter=0;iter<arrlength;iter++)
-            {
-                String chartData= chartDataArr[iter];
-                String stockValue= chartData.split(",")[1];
-                entries.add(new Entry( iter,  Float.parseFloat(stockValue)));
+            for (int iter = 0; iter < arrlength; iter++) {
+                String chartData = chartDataArr[iter];
+                String timeMilliSecondsStr = chartData.split(",")[0];
+
+                String stockValue = chartData.split(",")[1];
+
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy");
+                String date = simpleDateFormat.format(Long.parseLong(timeMilliSecondsStr));
+
+                entries.add(new Entry(iter, Float.parseFloat(stockValue)));
+                labelMap.put(iter, date);
             }
 
             LineDataSet dataSet = new LineDataSet(entries, getString(R.string.chart_label));
+            dataSet.setLineWidth(4f);
+            dataSet.setCircleRadius(5f);
+            lineData = new LineData(dataSet);
+            XAxis xAxis = lineChart.getXAxis();
+            xAxis.setValueFormatter(new MyCustomFormatter());
 
-           lineData = new LineData(dataSet);
             lineChart.setData(lineData);
 
             lineChart.invalidate();
@@ -130,5 +150,20 @@ public class DetailFragment extends Fragment implements  LoaderManager.LoaderCal
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
 
+    }
+
+    /**
+     * class to set labels of X Axis
+     */
+
+    private class MyCustomFormatter implements IAxisValueFormatter {
+
+        public MyCustomFormatter() {
+        }
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            return labelMap.get((int) value);
+        }
     }
 }
